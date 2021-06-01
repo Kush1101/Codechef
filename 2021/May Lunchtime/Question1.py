cf94d51f8bab1b672c4a88a7b4667465da984157
Kushagra Bansal <kushagrabansalajmer@gmail.com>
2021-06-01 16:20:32 +0530

    Create Question1.py

---
+++ 2021/May Lunchtime/Question1.py
@@ -0,0 +1,41 @@
+"""
+You and your friend are playing a game with hoops. There are N hoops (where N is odd) in a row. You jump into hoop 1, and your friend jumps into hoop N. Then you jump into hoop 2, and after that, your friend jumps into hoop N−1, and so on.
+
+The process ends when someone cannot make the next jump because the hoop is occupied by the other person. Find the last hoop that will be jumped into.
+
+Input
+The first line contains an integer T, the number of test cases. Then the test cases follow.
+Each test case contains a single line of input, a single integer N.
+Output
+For each testcase, output in a single line the answer to the problem.
+
+Constraints
+1≤T≤105
+1≤N<2⋅105
+N is odd
+
+2
+1
+3
+Sample Output
+1
+2
+Explanation
+Test Case 1: Since there is only 1 hoop, that's the only one to be jumped into.
+
+Test Case 2: The first player jumps into hoop 1. The second player jumps into hoop 3 and finally the first player jumps into hoop 2. Then the second player cannot make another jump, so the process stops.
+
+"""
+
+"""
+Since n is always odd, the last hoop that will be jumped in will be (n+1)//2
+"""
+t = int(input())
+for tc in range(t):
+  n = int(input())
+  print((n+1)//2)
+
+
+
+
+
